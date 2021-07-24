6c27bf7422d025cb25900b89cc56353d63b91882
jiang <392711804@qq.com>
2021-07-24 20:34:29 +0800

    modify init.el

--- init.el
+++ init.el
@@ -11,6 +11,12 @@
 ; activate all the packages
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(use-package try :ensure t)
+(use-package which-key :ensure t :config (which-key-mode))
+
 ; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -29,7 +35,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme evil)))
+ '(package-selected-packages
+   '(company-lsp lsp-ui ccls modern-cpp-font-lock flycheck auto-complete zenburn-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -46,3 +53,24 @@
 (set-face-attribute 'default nil :height 200)
 (global-linum-mode 1) ; always show line numbers                              
 (setq linum-format "%d| ") ; set format
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+; syntax checking
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+
+(use-package modern-cpp-font-lock
+:ensure t)
+
+(require 'init-lsp)
+(require 'init-gdb)
+
